0862ae0ca1a8a745c131ceb4f947c9d7c9eeacc0
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2023-10-18 22:51:44 +0200

    basic cleanup

--- todoist-sync.el
+++ todoist-sync.el
@@ -21,8 +21,9 @@
 
 
 ;; TODO: support for recurring tasks
-
-
+;; TODO: check for conflicts in the deadline and allow user to resolve them
+;; TODO: write file??? (or ask user if file should be written to)
+;; TODO: if called interactively ask to save changed files
 
 (require 'json)
 (require 'request)
@@ -44,27 +45,24 @@
 
 (defvar todoist-sync--api-url "https://api.todoist.com/sync/v9/sync")
 
-(defvar todoist-sync--agenda-uuid-cache nil "The UUID of the agenda project.")
-
+;; This gets cached before the first sync
+(defvar todoist-sync--agenda-uuid nil "The UUID of the agenda project.")
 
 (defun todoist-sync--get-sync-token ()
-  "Either returns the sync token or * if a full sync is needed."
+  "Get the sync token for the Todoist API. May return '*'
+   to indicate a full sync is configured."
   (if todoist-sync-incremental-sync
       todoist-sync--sync-token
     "*"))
 
-;; TODO: remove this again
-(defun todoist-sync--update-sync-token (new-token)
-  "Updates the sync token"
-  (setq todoist-sync--sync-token new-token))
+(defun todoist-sync--update-sync-token (response)
+  "Updates the sync token from a todoist response"
+  (setq todoist-sync--sync-token (cdr (assoc 'sync_token response))))
 
-(defun todoist-sync--generate-uuid ()
-  "Generate a random UUID."
-  (replace-regexp-in-string " " "-" (downcase (md5 (format "%s%s%s" (random) (current-time) user-mail-address)))))
+;; ================== API ==================
 
 (defun todoist-sync-get-projects (callback)
   "Get all projects from the Todoist API. Ignores sync token."
-  (message "Getting projects")
   (request
     todoist-sync--api-url
     :headers `(("Authorization" . ,(concat "Bearer " todoist-sync-token)))
@@ -73,16 +71,13 @@
     :parser 'json-read
     :success (cl-function
               (lambda (&key data &allow-other-keys)
-                (todoist-sync--update-sync-token (cdr (assoc 'sync_token data)))
                 (funcall callback (cdr (assoc 'projects data)))))
     :error (cl-function
             (lambda (&key data &allow-other-keys)
-              (message "Got error: %S" data)))
-    ))
+              (message "Got error: %S" data)))))
 
-;; DEPRECATED: the updated items will usually just be the response of the add-item request
-(defun todoist-sync-get-items (callback)
-  "Get all items from the Todoist API."
+(defun todoist-sync-get-items (callback &optional update-sync-token)
+  "Get all items changed since the last sync from the Todoist API."
   (request
     todoist-sync--api-url
     :headers `(("Authorization" . ,(concat "Bearer " todoist-sync-token)))
@@ -91,15 +86,21 @@
     :parser 'json-read
     :success (cl-function
               (lambda (&key data &allow-other-keys)
-                (todoist-sync--update-sync-token (cdr (assoc 'sync_token data)))
+                (if update-sync-token
+                    (todoist-sync--update-sync-token data))
                 (funcall callback (cdr (assoc 'items data)))))
     :error (cl-function
             (lambda (&key data &allow-other-keys)
               (message "Got error: %S" data)))))
 
-(defun todoist-sync-add-item (marker args agenda-uuid &optional sync-todo-state)
-  "Add an item to a project."
+(defun todoist-sync--filter-by-project (items project-uuid)
+  "Filter the list of items by the project UUID."
+  (seq-filter
+   (lambda (item)
+     (string= (cdr (assoc 'project_id item)) project-uuid))
+   items))
 
+(defun todoist-sync-add-item (args callback)
   (request
     todoist-sync--api-url
     :headers `(("Authorization" . ,(concat "Bearer " todoist-sync-token)))
@@ -111,85 +112,39 @@
                                              (uuid . ,(todoist-sync--generate-uuid))
                                              (args . ,args))))))
     :parser 'json-read
-    ;; TODO custom success / failure callbacks
     :success (cl-function
               (lambda (&key data &allow-other-keys)
-                (when sync-todo-state
-                  (message "Syncing todo state")
-                  (todoist-sync--update-sync-token (cdr (assoc 'sync_token data)))
-                  ;; NOTE: I'm not sure if there could be conflicts if the some heading is updated at the same time asynchronously (also not everything has an id yet ...)
-                  (todoist-sync--org-update-from-items
-                   (todoist-sync--filter-by-project
-                    (cdr (assoc 'items data))
-                    agenda-uuid)))
-                ;; TODO: [MULTIPLE] For now assume that only one item is added at a time.
-                (let* ((temp_id_mapping (car (cdr (assoc 'temp_id_mapping data))))
-                       (id (cdr temp_id_mapping)))
-                  (org-entry-put marker todoist-sync-org-prop-synced id)
-                  (message "Successfully added item %s" id))))
+                (funcall callback data)))
     :error (cl-function
             (lambda (&key data &allow-other-keys)
               (message "Got error: %S" data)))))
 
-(defun todoist-sync--extract-agenda-project-uuid (projects)
-  "Extract the UUID of the agenda project from the list of projects."
-  (cdr (assoc 'id (car (seq-filter
-                        (lambda (project)
-                          (string= (cdr (assoc 'name project)) todoist-sync-agenda-project))
-                        projects)))))
-
-(defun todoist-sync--filter-by-project (items project-uuid)
-  "Filter the list of items by the project UUID."
-  (seq-filter
-   (lambda (item)
-     (string= (cdr (assoc 'project_id item)) project-uuid))
-   items))
-
-
-(defun todoist-sync--ensure-agenda-uuid (callback)
-  (if todoist-sync--agenda-uuid-cache
-      (funcall callback todoist-sync--agenda-uuid-cache)
-    (todoist-sync-get-projects
-     (lambda (projects)
-       (let ((agenda-project-uuid (todoist-sync--extract-agenda-project-uuid projects)))
-         (setq todoist-sync--agenda-uuid-cache agenda-project-uuid)
-         (funcall callback agenda-project-uuid)))
-     )))
-
-
-;; @DEPRECATED the update should be done by the update function and when
-;; sending items.
-(defun todoist-sync-get-agenda-items (callback)
-  "Get all items from the agenda project."
-  (todoist-sync--ensure-agenda-uuid
-   (lambda (agenda-uuid)
-     (todoist-sync-get-items
-      (lambda (items)
-        (funcall callback (todoist-sync--filter-by-project items agenda-uuid))))
-     )))
-
-
 ;; ====== Org mode integration ======
 
 (defun todoist-sync--org-visit-todos (callback)
-  "Return a list of all (not done) TODO entries from all agenda files.
-Each element in the list is a cons cell (HEADING . FILENAME)."
+  "Visit all TODO, etc. entries from all agenda files and calls the
+   callback with the marker placed on the heading."
   (let* ((today (org-today))
          (date (calendar-gregorian-from-absolute today))
          (files (org-agenda-files nil 'ifmode))
          (rtnall nil))
     (while (setq file (pop files))
       (org-check-agenda-file file)
-      (let ((rtn (org-agenda-get-day-entries file date :todo)))
-        (setq rtnall (append rtnall rtn))))
+      (let ((old-regexp org-not-done-regexp))
+        (setq org-not-done-regexp org-todo-regexp)
+        (let ((rtn (org-agenda-get-day-entries file date :todo)))
+          (setq rtnall (append rtnall rtn)))
+        (setq org-not-done-regexp old-regexp)))
     (dolist (entry rtnall)
       (let ((marker (get-text-property 0 'org-hd-marker entry)))
         (with-current-buffer (marker-buffer marker)
           (goto-char marker)
           (funcall callback))))))
 
-;;
+;; ================== Sync Agenda Files ==================
+
 ;; TODO: do this properly
+;; TODO: remove DEADLINE from description
 (defun todoist-sync--clean-org-text (text)
   "Remove the PROPERTIES drawer from the org text."
   (replace-regexp-in-string ":PROPERTIES:\\(.*\n\\)*?:END:\n" "" text))
@@ -203,70 +158,91 @@ Each element in the list is a cons cell (HEADING . FILENAME)."
   "Converts <2023-10-09 Sun> to 2023-10-09"
   (replace-regexp-in-string "<\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\).*>" "\\1" org-time-str))
 
-(defun todoist-sync-push-agenda-todos (&optional sync-todo-state)
-  "Push all TODO entries from all agenda files to todoist."
-  (todoist-sync--org-visit-todos
-   (lambda ()
-     (let* ((synced (org-entry-get (point) todoist-sync-org-prop-synced))
-            (heading (org-get-heading t t t t))
-            (description (todoist-sync--clean-org-text (org-get-entry)))
-            (org-due-str (org-entry-get (point) "DEADLINE"))
-            (todoist-due-string (when org-due-str (todoist-sync--org-to-todoist-date org-due-str)))
-            ;; TODO: Use this when directly assigning the time
-            ;; (due (when todoist-due-string `((date . ,todoist-due-string))))
-            (due (when todoist-due-string `((string . ,todoist-due-string)))))
-       (when (not synced)
-         ;; TODO: [MULTIPLE] collect all items and send them in one request
-         (todoist-sync--ensure-agenda-uuid
-          (lambda (agenda-uuid)
-            (todoist-sync-add-item
-             (point-marker) `((content . ,heading)
-                              (project_id . ,agenda-uuid)
-                              (description . ,description)
-                              (due . ,due))
-             agenda-uuid
-             sync-todo-state))))))))
+(defun todoist-sync--todoist-date-for-at-point ()
+  (let* ((org-due-str (org-entry-get (point) "DEADLINE"))
+         (todoist-due-string
+          (when org-due-str (todoist-sync--org-to-todoist-date org-due-str))))
+    (when todoist-due-string `((string . ,todoist-due-string)))))
 
+(defun todoist-sync--generate-uuid ()
+  "Generate a random UUID."
+  (replace-regexp-in-string " " "-" (downcase (md5 (format "%s%s%s" (random) (current-time) user-mail-address)))))
 
-(defun todoist-sync--org-do-update (id item)
-  "Called for each org heading where the todoist item has changed."
-  ;; At the moment the cursor is placed at that position ... TODO: pass marker insead
-  (let* ((org-is-done (org-entry-is-done-p))
-         (todoist-is-done (cdr (assoc 'completed_at item))))
-    (if (and (not org-is-done) todoist-is-done)
-        (org-todo 'done))
-    ;; BUG: can't be visited because the org-crawling function ignores tasks that are done
-    (if (and org-is-done (not todoist-is-done))
-        (message "%s (%s) is done in org but not in todoist." (org-get-heading t t t t) id))))
+(defun todoist-sync--extract-agenda-project-uuid (projects)
+  "Extract the UUID of the agenda project from the list of projects."
+  (cdr (assoc 'id (car (seq-filter
+                        (lambda (project)
+                          (string= (cdr (assoc 'name project)) todoist-sync-agenda-project))
+                        projects)))))
 
+;; Used in all entry points to the package.
+(defun todoist-sync--ensure-agenda-uuid (callback)
+  "Makes sure that the agenda project UUID is cached.
+   Then calls the callback with the UUID as argument."
+  (if todoist-sync--agenda-uuid
+      (funcall callback)
+    (todoist-sync-get-projects
+     (lambda (projects)
+       (let ((agenda-project-uuid (todoist-sync--extract-agenda-project-uuid projects)))
+         (setq todoist-sync--agenda-uuid agenda-project-uuid)
+         (funcall callback))))))
 
-(defun todoist-sync--org-update-from-items (items)
-  "Update the org entry with the data from the Todoist API."
-  (message "Updating org from todoist:\n%s" items)
-  (todoist-sync--ensure-agenda-uuid
-   (lambda (agenda-uuid)
-     (let* ((agenda-items (todoist-sync--filter-by-project items agenda-uuid))
-            (agenda-items-by-id (mapcar (lambda (item) (cons (cdr (assoc 'id item)) item)) agenda-items)))
-       (if agenda-items
-           (todoist-sync--org-visit-todos
-            (lambda ()
-              (let* ((id (org-entry-get (point) todoist-sync-org-prop-synced))
-                     (item (cdr (assoc id agenda-items-by-id))))
-                (if item (todoist-sync--org-do-update id item)))))
-         (message "Nothing changed in todoist."))))))
+;; Gets all updated items from the agenda project
+(defun todoist-sync--get-agenda-udpates (callback)
+  "Get all items from the agenda project.
+   Calls the callback with the changed items as argument."
+  (todoist-sync-get-items
+   (lambda (items)
+     (funcall callback (todoist-sync--filter-by-project items todoist-sync--agenda-uuid)))
+   t))
 
-(defun todoist-sync-sync-todo-state ()
-  "Download the todo state from todoist and update the org files."
-  (interactive)
-  (todoist-sync-get-agenda-items 'todoist-sync--org-update-from-items))
+(defun todoist-sync--visit-org-heading (changed-agenda-items-by-id)
+  (let ((synced-id (org-entry-get (point) todoist-sync-org-prop-synced))
+        (heading (org-get-heading t t t t))
+        (description (todoist-sync--clean-org-text (org-get-entry)))
+        (due (todoist-sync--todoist-date-for-at-point))
+        (org-is-done (org-entry-is-done-p)))
+    (when synced-id
+      ;; If the item is already synced we need to check if it has changed
+      (let* ((todoist-item (cdr (assoc synced-id changed-agenda-items-by-id)))
+             (todoist-is-done (cdr (assoc 'completed_at todoist-item))))
+        (when org-is-done
+          ;; TODO: push change to todoist
+          (message "org is done but this has not been pushed to todoist."))
+        (when todoist-is-done
+          (org-todo 'done)
+          ;; TODO: handle cancled etc better
+          (org-entry-delete (point-marker) todoist-sync-org-prop-synced))))
+    (when (and (not synced-id) (not org-is-done))
+      ;; TODO: [MULTIPLE] collect all items and send them in one request
+      (todoist-sync-add-item
+       `((content . ,heading)
+         (project_id . ,todoist-sync--agenda-uuid)
+         (description . ,description)
+         (due . ,due))
+       (lambda (data)
+         ;; Add the id of the new item to the org entry
+         (let* ((temp_id_mapping (car (cdr (assoc 'temp_id_mapping data))))
+                (id (cdr temp_id_mapping)))
+           (org-entry-put (point-marker) todoist-sync-org-prop-synced id)
+           (message "Successfully added item %s" id)))))))
 
+(defun todoist-sync--do-sync ()
+  "Sync the agenda with todoist."
+  (todoist-sync--get-agenda-udpates
+   (lambda (items)
+     (let ((agenda-items-by-id (mapcar (lambda (item) (cons (cdr (assoc 'id item)) item)) items)))
+       (todoist-sync--org-visit-todos
+        (lambda ()
+          (todoist-sync--visit-org-heading agenda-items-by-id)))))))
 
-;; TODO: this only syncs if at least one item is added
+;; ==================== Refactored up to here ====================
+
+;; The only public function in the package
 (defun todoist-sync ()
   "Sync the agenda with todoist."
   (interactive)
-  (todoist-sync-push-agenda-todos t)
-  (todoist-sync-sync-todo-state))
+  (todoist-sync--ensure-agenda-uuid #'todoist-sync--do-sync))
 
 (provide 'todoist-sync)
 ;;; todoist-sync.el ends here
